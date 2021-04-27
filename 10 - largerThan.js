96349c0924f8504fdab0b9d9412ffc18c920a6eb
Aline Dubois <juillet.aline@gmail.com>
2021-04-27 18:38:17 +0200

    avancée des exercices d'algo

--- 10 - largerThan.js
+++ 10 - largerThan.js
@@ -1,7 +1,8 @@
 // create a function largerThan that will receive 2 numbers as parameters and return the largest number only
 
-const largerThan = () => {
-  
+const largerThan = (a, b) => {
+    return Math.max(a, b);
+
 };
 
 
--- 12 - luckyNumber.js
+++ 12 - luckyNumber.js
@@ -1,10 +1,17 @@
-// create a function luckyNumber that will receive a number as a parameter and return if it is a lucky number or not. A lucky number must match these conditions:
+// create a function luckyNumber that will receive a number as a parameter and return if it is a lucky number or not.
+// A lucky number must match these conditions:
 // it is positive
 // it is a multiple of 2 or 3
 // it is not 15
 
-const luckyNumber = () => {
- 
+const luckyNumber = (number) => {
+    if (number > 0 && (number % 2 === 0 || number % 3 === 0) && number !== 15) {
+
+        return "Is a lucky number";
+    } else {
+        return "Is not a lucky number";
+    }
+
 };
 
 console.log(luckyNumber(15));
