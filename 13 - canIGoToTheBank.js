690a85d0f18de866c83bc5a789ef76adb6eff29c
Aline Dubois <juillet.aline@gmail.com>
2021-04-29 17:21:48 +0200

    avancée des exercices d'algo

--- 13 - canIGoToTheBank.js
+++ 13 - canIGoToTheBank.js
@@ -1,8 +1,18 @@
-// create a canIGoToTheBank function that will receive 2 parameters: the day of the week and the time. The function must return true only if the bank is still open.
+// create a canIGoToTheBank function that will receive 2 parameters: the day of the week and the time.
+// The function must return true only if the bank is still open.
 // keep in mind that the bank is open from Monday through Friday and from 10:00 until 18:00
 
-const canIGoToTheBank = () => {
+const open = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+const hour = [10, 11, 12,13, 14, 15, 16, 17, 18];
+
+const canIGoToTheBank = (day, time) => {
+
+    if (open.includes(day) && hour.includes(time)){
+        return true;
+    } else {
+        return false;
+    }
   
 };
 
-console.log(canIGoToTheBank("Sunday", 10));
+console.log(canIGoToTheBank("saturday", 10));
